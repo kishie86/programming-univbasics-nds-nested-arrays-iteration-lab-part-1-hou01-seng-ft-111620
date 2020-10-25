6875f011d9b1e359dd2c8bd9ecb2cd70d3acec3e
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-25 19:04:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,11 +7,11 @@ def find_even_values(src)
 
 
 row_index = 0
-while row_index < spice_rack.count do
+while row_index < src.count do
   element_index = 0
-  while element_index < spice_rack[row_index].count do
-    p spice_rack[row_index][element_index]
+  while element_index < src[row_index].count do
+    p src[row_index][element_index]
     element_index += 1
   end
   row_index += 1
-end #=> nil
+end
